b1f36aeb8419fa6d200a689f74b7466252696c7b
bauda04 <antenainarandrianantoandro@gmail.com>
2023-03-07 21:12:45 +0300

    Création d'un bean géré par CDI (backing bean de la page customerList.xhtml) 📦️

---
+++ src/main/java/mg/itu/tpCustomerAntenainaRANDRIANANTOANDRO/jsf/CustomerBean.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpCustomerAntenainaRANDRIANANTOANDRO.jsf;
+
+import jakarta.ejb.EJB;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpCustomerAntenainaRANDRIANANTOANDRO.ejb.CustomerManager;
+import mg.itu.tpCustomerAntenainaRANDRIANANTOANDRO.entities.Customer;
+
+/**
+ *
+ * @author Antenaina
+ */
+/**
+ * Backing bean de la page customerList.xhtml.
+ */
+@Named(value = "customerBean")
+@ViewScoped
+public class CustomerBean implements Serializable {
+
+    /**
+     * Creates a new instance of CustomerBean
+     */
+    private List<Customer> customerList;
+    @EJB
+    private CustomerManager customerManager;
+
+    public CustomerBean() {
+    }
+
+    /**
+     * Retourne la liste des clients pour affichage dans une DataTable.
+     */
+    public List<Customer> getCustomers() {
+        if (customerList == null) {
+            customerList = customerManager.getAllCustomers();
+        }
+        return customerList;
+    }
+}
